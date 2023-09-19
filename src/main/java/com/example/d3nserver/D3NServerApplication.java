f7fc91ebfa34179f7f34b423efc2480a16f46c41
Y-Joo <wldudwn99@gmail.com>
2023-09-19 12:56:34 +0900

    Chore: Project Setting

---
+++ src/main/java/com/example/d3nserver/D3NServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.d3nserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D3NServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(D3NServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/d3nserver/D3NServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.d3nserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D3NServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
