71a66b4b6b4c0bb5547ea67253a9251e4f3932cf
Y-Joo <wldudwn99@gmail.com>
2023-09-19 16:51:35 +0900

    Feat: Quiz 생성

---
+++ src/main/java/com/example/d3nserver/quiz/domain/Quiz.java
@@ -0,0 +1,24 @@
+package com.example.d3nserver.quiz.domain;
+
+import com.example.d3nserver.common.BaseEntity;
+import com.example.d3nserver.news.domain.News;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.util.List;
+
+@Entity
+@Getter
+public class Quiz extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @ElementCollection
+    private List<String> choiceList;
+    private Integer answer;
+    @ManyToOne
+    @JsonBackReference
+    @JoinColumn(name="news_id")
+    private News news;
+}
