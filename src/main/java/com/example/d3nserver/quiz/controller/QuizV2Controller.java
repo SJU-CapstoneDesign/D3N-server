ec4e8a10ddf5cc66a984c17ae5bce86ecbb4e577
Y-Joo <wldudwn99@gmail.com>
2023-11-02 14:28:03 +0900

    Refactor: 퀴즈 v1.1 컨트롤러 작성

---
+++ src/main/java/com/example/d3nserver/quiz/controller/QuizV2Controller.java
@@ -0,0 +1,37 @@
+package com.example.d3nserver.quiz.controller;
+
+import com.example.d3nserver.auth.jwt.ReqUser;
+import com.example.d3nserver.common.base.BaseResponse;
+import com.example.d3nserver.quiz.dto.QuizResponseDto;
+import com.example.d3nserver.quiz.dto.SolvedQuizRequestDto;
+import com.example.d3nserver.quiz.dto.SolvedQuizResponseDto;
+import com.example.d3nserver.quiz.service.QuizService;
+import com.example.d3nserver.quiz.service.SolvedQuizService;
+import com.example.d3nserver.user.domain.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Tag(name = "Quiz v2 API", description = "Quiz 관련 api")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value="/api/v1.1/quiz")
+public class QuizV2Controller {
+    private final QuizService quizService;
+    private final SolvedQuizService solvedQuizService;
+
+    @Operation(summary = "Quiz list", description = "뉴스 id를 입력받아 해당하는 뉴스의 퀴즈 리스트를 반환한다.")
+    @GetMapping(value = "/list")
+    public BaseResponse<List<QuizResponseDto>> getQuizList(@RequestParam @Parameter(description="뉴스 id")Long newsId){
+        return BaseResponse.ofSuccess(quizService.getQuizList(newsId));
+    }
+
+    @PostMapping(value = "/list/submit")
+    public BaseResponse<List<SolvedQuizResponseDto>> submitQuiz(@ReqUser User user, @RequestBody List<SolvedQuizRequestDto> solvedQuizRequestDtoList){
+        return BaseResponse.ofSuccess(solvedQuizService.saveSolvedQuizList(user, solvedQuizRequestDtoList));
+    }
+}
