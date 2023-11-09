1c5bf6011fe6659469f52357baaff8b69ab28315
ms0828 <musetng1@naver.com>
2023-11-10 00:02:02 +0900

    setQuizAnswerAndSelectedAnswer

--- src/main/java/com/example/d3nserver/news/service/NewsService.java
+++ src/main/java/com/example/d3nserver/news/service/NewsService.java
@@ -5,6 +5,7 @@ import com.example.d3nserver.news.dto.NewsDTO;
 import com.example.d3nserver.news.domain.News;
 import com.example.d3nserver.news.dto.NewsResponseDto;
 import com.example.d3nserver.news.repository.NewsRepository;
+import com.example.d3nserver.quiz.dto.response.QuizResponseDto;
 import com.example.d3nserver.quiz.service.QuizService;
 import com.example.d3nserver.time.domain.NewsReadingTime;
 import com.example.d3nserver.time.repository.NewsReadingTimeRepository;
@@ -16,6 +17,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -41,10 +43,12 @@ public class NewsService {
     }
 
     private NewsResponseDto getResponseDto(User user, News news){
-        NewsResponseDto responseDto = new NewsResponseDto(news);
-        Optional<NewsReadingTime> newsReadingTime = newsReadingTimeRepository.findByUserIdAndNewsId(user.getId(),news.getId());
-        newsReadingTime.ifPresent(value -> responseDto.setSecondTime(value.getSecondTime()));
-        
-        return responseDto;
+        NewsResponseDto newsResponseDto = new NewsResponseDto(news);
+        Optional<NewsReadingTime> newsReadingTime = newsReadingTimeRepository.findByUserIdAndNewsId(user.getId(), news.getId());
+        newsReadingTime.ifPresent(value -> newsResponseDto.setSecondTime(value.getSecondTime()));
+        List<QuizResponseDto> quizResponseDtoList = quizService.getQuizListByUser(user, news.getId());
+        newsResponseDto.setQuizAnswerList(quizResponseDtoList.stream().map(QuizResponseDto::getAnswer).collect(Collectors.toList()));
+        newsResponseDto.setSelectedAnswerList(quizResponseDtoList.stream().map(QuizResponseDto::getSelectedAnswer).collect(Collectors.toList()));
+        return newsResponseDto;
     }
 }
